37908350d89128c2741b3f41d070057955005bb3
LebedevaAlexandraVictorovna <avlebedeva_4@miem.hse.ru>
2020-04-16 20:56:41 +0300

    Create discipline.py
    Здесь описан класс Discipline

---
+++ discipline.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+class Discipline():  # можно и препода добавить
+    def __init__(self):
+        self.name = "Название дисциплины"
+        self.course = 0  # на каком курсе проходят дисциплину
+        self.credits = 5
+        self.__vedomost = ""  # файл с оценками
+    # setters
+    def set_name(self, name):
+        self.name = name
+    
+    def set_course(self, course):
+        self.course = course
+        
+    def set_credits(self, credit):
+        self.credits = credit
+    
+    def set_vedomost(self, ved):
+        self.__vedomost = ved
+
+    # getters
+    def get_name(self):
+        return(self.name)
+    
+    def get_course(self):
+        return(self.course)
+    
+    def get_credits(self):
+        return(self.credits)
+    
+    def get_vedomost(self):
+        return(self.__vedomost)
+    
+    # нормальные методы
+    def find_mark(self, student):
+        f = open(self.get_vedomost())
+        text = f.read().splitlines()
+        for line in text:
+            if student in line:
+                mark = line[-2:]
+                if mark != "10":
+                    mark = mark[-1]
+                break
+        f.close()
+        return(mark)
