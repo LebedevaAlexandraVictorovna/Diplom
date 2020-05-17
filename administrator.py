9d94bd1ebcc1dfa32fc393caf0f5c2aebc18acc7
LebedevaAlexandraVictorovna <avlebedeva_4@miem.hse.ru>
2020-05-17 22:23:01 +0300

    Create administrator.py

---
+++ administrator.py
@@ -0,0 +1,36 @@
+class Administrator():   # наследование с student?
+    def __init__(self):
+        self.surname = "Фамилия"
+        self.name = "Имя"
+        self.patronym = "Отчество"
+        self.login = "admin" # логин всегда admin, setter не нужен
+        self.__password = ""
+    
+    # setters
+    def set_surname(self, s):
+        self.surname = s
+    
+    def set_name(self, n):
+        self.name = n
+
+    def set_patronym(self, p):
+        self.patronym = p
+    
+    def set_password(self, password):
+        self.__password = password
+    
+    # getters
+    def get_surname(self):
+        return self.surname
+
+    def get_name(self):
+        return self.name
+
+    def get_patronym(self):
+        return self.patronym
+    
+    def get_login(self):
+        return self.login
+    
+    def get_password(self):
+        return self.__password
