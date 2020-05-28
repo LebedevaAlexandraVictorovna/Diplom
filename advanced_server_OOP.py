99b1689ebc281c49c2ffb83fd93650396e22473b
LebedevaAlexandraVictorovna <avlebedeva_4@miem.hse.ru>
2020-05-28 17:12:42 +0300

    decorator for ocket

--- advanced_server_OOP.py
+++ advanced_server_OOP.py
@@ -5,8 +5,12 @@ from threading import Thread
 import random
 import boto3
 
+class Okt():  # ocket interface
+    def read(self):
+        pass
 
-class Ocket:
+
+class Ocket(Okt):
     def __init__(self, resrc, bucket_name, file_name):
         self.resrc = resrc
         self.bucket_name = bucket_name
@@ -20,6 +24,45 @@ class Ocket:
         self.obj.put(Body = data)
 
 
+class Decorator(Okt):  
+    """
+    Базовый класс Декоратора следует тому же интерфейсу, что и другие
+    компоненты. Основная цель этого класса - определить интерфейс обёртки для
+    всех конкретных декораторов. Реализация кода обёртки по умолчанию может
+    включать в себя поле для хранения завёрнутого компонента и средства его
+    инициализации.
+    """
+
+    _component: Okt = None
+
+    def __init__(self, component: Okt) -> None:
+        self._component = component
+
+    @property
+    def component(self) -> str:
+        """
+        Декоратор делегирует всю работу обёрнутому компоненту.
+        """
+
+        return self._component
+
+    def read(self) -> str:
+        return self._component.read()
+
+class DecodeDecorator(Decorator):  # read с декодом
+    """
+    Конкретные Декораторы вызывают обёрнутый объект и изменяют его результат
+    некоторым образом.
+    """
+
+    def read(self) -> str:
+        return (self.component.read()).decode('utf-8')
+
+class SplitlinesDecorator(Decorator):
+
+    def read(self) -> str:
+        return (self.component.read()).splitlines()
+
 class Bucket:
     def __init__(self, resrc, bucket_name):
         self.bucket_name = bucket_name
@@ -31,6 +74,11 @@ class Bucket:
     def ocket(self, file_name) -> Ocket:
         ocket = Ocket(self.resrc, self.bucket_name, file_name)
         return ocket
+
+
+def client_code(component: Okt) -> None:
+
+    print(f"RESULT: {component.read()}", end="")
 
 
 class Resource:
@@ -46,5 +94,14 @@ class Resource:
 resource = Resource()
 bucket = resource.bucket('liststudentsadmins')
 ocket = bucket.ocket('list.txt')
-content = ocket.read()
-print(content)
+#content = ocket.read()
+#print(content)
+
+print("Client: I've got a simple component:")
+client_code(ocket)
+print("\n")
+decorator1 = DecodeDecorator(ocket)
+decorator2 = SplitlinesDecorator(decorator1)
+print("Client: Now I've got a decorated component:")
+client_code(decorator2)
+print("\n")
